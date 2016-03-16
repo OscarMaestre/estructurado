daf760fe81e162cafd161b442c23c4204efbcdb7
oscar <oscar@example.com>
2016-03-16 09:25:31 +0100

    Extrayendo con modelos

--- gestion/extractor_adjudicaciones/extractor.py
+++ gestion/extractor_adjudicaciones/extractor.py
@@ -4,6 +4,8 @@
 from utilidades.ficheros.ProcesadorPDF import ProcesadorPDF
 from utilidades.basedatos.Configurador import Configurador
 from utilidades.basedatos.GestorBD import GestorBD
+from utilidades.fechas.GestorFechas import GestorFechas
+from utilidades.ficheros.GestorFicheros import GestorFicheros
 
 import os
 import sys
@@ -13,11 +15,17 @@ configurador=Configurador (os.path.sep.join ([".."]) )
 configurador.activar_configuracion ( "gestion.settings")
 from modelado_bd.models import *
 
-
 from datetime import datetime
+
+
 i=1
 max_filas=300
 sql_intermedio=""
+
+sufijo_fecha = sys.argv[1]
+sufijo_fecha = sufijo_fecha.replace('-', '')
+prefijo_funcion = 'fun_' + sufijo_fecha + '_'
+num_funcion=1
 
 nombramientos=Nombramiento.objects.all()
 for nombramiento in nombramientos:
@@ -27,7 +35,7 @@ for nombramiento in nombramientos:
     #print(codigo_centro)
     if i % max_filas == 0:
         nombre_funcion = prefijo_funcion + str(num_funcion)
-        imprimir = GestorDB.get_procedimiento(nombre_funcion,
+        imprimir = GestorBD.get_procedimiento(nombre_funcion,
                 sql_intermedio)
         print (imprimir)
         num_funcion = num_funcion + 1
@@ -48,19 +56,23 @@ for nombramiento in nombramientos:
         + "' where dni='" + nombramiento.nif + "'"
     sql_intermedio += GestorBD.crear_sentencia_update(temp)
     
-    fecha_inicio=utilidades.convertir_fecha_de_iso_a_estandar(fila[4])
-    fecha_fin=utilidades.convertir_fecha_de_iso_a_estandar(fila[5])
-    if fecha_hoy<fecha_final_contrato:
-        descripcion_fechas = 'Desde ' + fecha_inicio + ' hasta ' + fecha_fin \
-        + ' (' + fila[3] + ')'
+    fecha_inicio    =   nombramiento.fecha_inicio
+    fecha_fin       =   nombramiento.fecha_fin 
+    if fecha_hoy<fecha_fin:
+        descripcion_fecha_inicio=nombramiento.fecha_inicio.strftime("%d-%m-%Y")
+        descripcion_fecha_fin   =nombramiento.fecha_fin.strftime("%d-%m-%Y")
+        descripcion_fechas = 'Desde ' + descripcion_fecha_inicio + \
+                             ' hasta ' + descripcion_fecha_fin \
+        + ' (' + nombramiento.especialidad.descripcion + ')'
     else:
-        descripcion_fechas = 'En paro, su ultimo contrato acabó el ' + fecha_fin 
+        descripcion_fecha_fin=fecha_fin.strftime("%d-%m-%Y")
+        descripcion_fechas = 'En paro, su ultimo contrato acabó el ' + descripcion_fecha_fin
     temp = "update gaseosa set auxiliar='" + descripcion_fechas \
-        + "' where dni='" + fila[0] + "'"
-    sql_intermedio += GestorDB.crear_sentencia_update(temp)
+        + "' where dni='" + nombramiento.nif + "'"
+    sql_intermedio += GestorBD.crear_sentencia_update(temp)
     i = i + 1
 nombre_funcion = prefijo_funcion + str(num_funcion)
-imprimir = GestorDB.get_procedimiento(nombre_funcion, sql_intermedio)
+imprimir = GestorBD.get_procedimiento(nombre_funcion, sql_intermedio)
 print (imprimir)
 num_funcion = num_funcion + 1
 nombre_funcion = prefijo_funcion + str(num_funcion + 1)+"\r\n"
@@ -70,4 +82,5 @@ for num in range(1, num_funcion):
 llamadas += 'End Function\r\n'
 print (llamadas)
 
-utilidades.anadir_a_fichero(nombre_funcion, 'llamadas_generales')
+gestor_ficheros=GestorFicheros()
+gestor_ficheros.anadir_a_fichero(nombre_funcion, 'llamadas_generales')
