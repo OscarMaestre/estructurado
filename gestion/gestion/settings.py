d7b5a741077254d7430ee2fd6f373cd553f8a660
oscar <oscar@example.com>
2016-04-06 09:16:06 +0200

    Calculadora v0

--- gestion/gestion/settings.py
+++ gestion/gestion/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'index',
+    'intcalc',
     'traslados',
     'modelado_bd',
     'documentos',
---
+++ gestion/intcalc/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class IntcalcConfig(AppConfig):
+    name = 'intcalc'
