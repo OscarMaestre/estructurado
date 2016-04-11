b6050ca5ce5bbfb1f24bdd7173e244ce17ab71ad
oscar <oscar@example.com>
2016-04-11 09:26:12 +0200

    Todas prov

--- gestion/html_disponibles/GeneradorLista.py
+++ gestion/html_disponibles/GeneradorLista.py
@@ -5,7 +5,7 @@ from django.template.loader import render_to_string, get_template
 from django.template.exceptions import TemplateDoesNotExist
 from django.db.models import Q
 from django.conf import settings
-
+from copy import copy
 import sys
 configurador=Configurador ( ".." )
 configurador.activar_configuracion ( "gestion.settings" )
@@ -18,15 +18,33 @@ POS_CU=2
 POS_GU=3
 POS_TO=4
 
+
+class Posiciones(object):
+    def __init__(self):
+        self.POS_AB_SIN_IDIOMA  =   1
+        self.POS_CR_SIN_IDIOMA  =   1
+        self.POS_CU_SIN_IDIOMA  =   1
+        self.POS_GU_SIN_IDIOMA  =   1
+        self.POS_TO_SIN_IDIOMA  =   1
+        
+        self.POS_AB_CON_INGLES  =   1
+        self.POS_CR_CON_INGLES  =   1
+        self.POS_CU_CON_INGLES  =   1
+        self.POS_GU_CON_INGLES  =   1
+        self.POS_TO_CON_INGLES  =   1
+        
+        self.POS_AB_CON_FRANCES  =   1
+        self.POS_CR_CON_FRANCES  =   1
+        self.POS_CU_CON_FRANCES  =   1
+        self.POS_GU_CON_FRANCES  =   1
+        self.POS_TO_CON_FRANCES  =   1
+        
+        
+        
 class Entrada(object):
-    def __init__(self, numero, dni,
-                 peticiones, posiciones, con_ingles, con_frances):
-        self.numero         =   numero
-        self.dni            =   dni
-        self.posiciones     =   posiciones
-        self.peticiones     =   peticiones
-        self.con_ingles     =   con_ingles
-        self.con_frances    =   con_frances
+    def __init__(self, persona, posiciones):
+        self.persona=persona
+        self.posiciones=copy ( posiciones )
         
 class GeneradorLista(object):
     def __init__(self, ClasePersona):
@@ -34,6 +52,9 @@ class GeneradorLista(object):
         
     def get_personas_por_orden_bolsa(self,
             fichero_plantilla, codigo_especialidad):
+        
+        posiciones = Posiciones()
+        
         personas=self.ClasePersona.objects.all().order_by("orden_bolsa")
         #print (dir(personas[0]))
         personas=self.ClasePersona.objects.filter(
@@ -41,28 +62,41 @@ class GeneradorLista(object):
         #print (personas[0])
         especialidad="0590"
         resultados=[]
-        pos_ab=0
-        pos_cr=0
-        pos_cu=0
-        pos_gu=0
-        pos_to=0
+        
         for p in personas:
             entrada=Entrada(
-                p.orden_bolsa, p.dni,
-                [p.elige_ab, p.elige_cr, p.elige_cu, p.elige_gu, p.elige_to],
-                [pos_ab, pos_cr, pos_cu, pos_gu, pos_to],
-                p.ingles, p.frances
+                p, posiciones
             )
             if p.elige_ab:
-                pos_ab=pos_ab+1
+                posiciones.POS_AB_SIN_IDIOMA+=1
+                if p.ingles:
+                    posiciones.POS_AB_CON_INGLES+=1
+                if p.frances:
+                    posiciones.POS_AB_CON_FRANCES+=1
             if p.elige_cr:
-                pos_cr=pos_cr+1
+                posiciones.POS_CR_SIN_IDIOMA+=1
+                if p.ingles:
+                    posiciones.POS_CR_CON_INGLES+=1
+                if p.frances:
+                    posiciones.POS_CR_CON_FRANCES+=1
             if p.elige_cu:
-                pos_cu=pos_cu+1
+                posiciones.POS_CU_SIN_IDIOMA+=1
+                if p.ingles:
+                    posiciones.POS_CU_CON_INGLES+=1
+                if p.frances:
+                    posiciones.POS_CU_CON_FRANCES+=1
             if p.elige_gu:
-                pos_gu=pos_gu+1
+                posiciones.POS_GU_SIN_IDIOMA+=1
+                if p.ingles:
+                    posiciones.POS_GU_CON_INGLES+=1
+                if p.frances:
+                    posiciones.POS_GU_CON_FRANCES+=1
             if p.elige_to:
-                pos_to=pos_to+1
+                posiciones.POS_TO_SIN_IDIOMA+=1
+                if p.ingles:
+                    posiciones.POS_TO_CON_INGLES+=1
+                if p.frances:
+                    posiciones.POS_TO_CON_FRANCES+=1
             resultados.append(entrada)
         contexto={
             "personas":resultados,
@@ -75,4 +109,4 @@ class GeneradorLista(object):
             
 if __name__ == '__main__':
     g=GeneradorLista(InterinoDisponible)
-    g.get_personas_por_orden_bolsa("html_disponibles/plantilla_lista.html", "0590001")
+    g.get_personas_por_orden_bolsa("html_disponibles/plantilla_lista.html", "0597031")
